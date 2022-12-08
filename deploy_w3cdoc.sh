9df88abac31a86ef84ba16243c8903fa39ee68ce
macbook <macbook@macbookdeMacBook.local>
2022-12-09 00:14:58 +0800

    temp

--- deploy_w3cdoc.sh
+++ deploy_w3cdoc.sh
@@ -3,7 +3,7 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+push_addr=git@github.com:chalecao/w3cpost.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 commit_info=`git describe --all --always --long`
 dist_path=public # 打包生成的文件夹路径
 push_branch=gh-pages # 推送的分支
